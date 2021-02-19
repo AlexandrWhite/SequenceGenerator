8862a2c62590bdb6e6bf302f76da22b9b0d82ec7
Alexandr White <sashabelyy123@gmail.com>
2021-02-19 21:40:39 +0500

    Работает случайное заполнение Исправлена работа с файлом linearGenerator

--- Sequence Generator/Matrix.cs
+++ Sequence Generator/Matrix.cs
@@ -157,14 +157,14 @@ namespace Sequence_Generator
             sr.ReadLine();
         }
 
-        public void RandomFill()
+        public void RandomFill(int maxValue)
         {
             Random rand = new Random();
             for(int i = 0; i < Rows.Count; i++)
             {
                 for(int j = 0; j < Columns.Count; j++)
                 {
-                    Rows[i][j] = rand.Next(0, 1001);
+                    Rows[i][j] = rand.Next(0, maxValue+1);
                 }
             }
         }
